949345ad12955a7609885b3fbd8921da9e0e944d
Prakash Khude <prakash_khude@yahoo.com>
2025-04-27 13:53:10 -0400

    First Draft Version

---
+++ netflix-gpt/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+   return (
+    <>
+      <div className="text 3xl font-bold text-red-500 text-center">
+       Hello world
+      </div>      
+    </>
+  )
+}
+
+export default App
